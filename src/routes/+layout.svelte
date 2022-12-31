fbd3c7e396411f2c086de86086ca0ef92029b78f
Eric Echemane <eechemane29@gmail.com>
2022-12-31 23:43:13 +0800

    navbar is now responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	//import type { LayoutServerData } from './$types';
 	//export let data: LayoutServerData;
+
+	const toggleNavOnMobile = () => {
+		document.getElementById('nav')?.classList.toggle('open');
+	};
 </script>
 
 <nav>
@@ -8,24 +12,33 @@
 		<img src="/favicon.png" alt="kuta balwarte logo" />
 		<h2>Kuta Balwarte</h2>
 	</section>
-	<section class="navigation">
+	<section class="navigation" id="nav">
 		<a href="/"> <h4>Home</h4> </a>
 		<a href="/menu"> <h4>Menu</h4> </a>
 		<a href="/order"> <h4>Orders</h4> </a>
-		<div class="input-field">
-			<span class="material-symbols-outlined"> search </span>
-			<input type="search" placeholder="Search your favorite dish" />
+		<div class="inputs">
+			<div class="input-field">
+				<span class="material-symbols-outlined"> search </span>
+				<input type="search" placeholder="Search your favorite dish" />
+			</div>
+			<button class="icon-btn">
+				<span class="material-symbols-outlined"> shopping_basket </span>
+				<sup class="badge">7</sup>
+			</button>
 		</div>
-		<button class="icon-btn">
-			<span class="material-symbols-outlined"> shopping_basket </span>
-			<sup class="badge">7</sup>
-		</button>
 	</section>
+	<button class="icon-btn" id="menu-open-btn" on:click={toggleNavOnMobile}>
+		<span class="material-symbols-outlined"> menu_open </span>
+	</button>
 </nav>
 
 <!--<slot />-->
 <style>
+	#menu-open-btn {
+		display: none;
+	}
 	nav {
+		--height: 120px;
 		max-width: 1080px;
 		margin: auto;
 		display: flex;
@@ -42,10 +55,48 @@
 		width: 3rem;
 	}
 
+	.inputs {
+		display: flex;
+		align-content: center;
+		gap: 2rem;
+	}
+
 	/* ================================ */
 	.navigation {
 		display: flex;
 		align-items: center;
 		gap: 2rem;
+	}
+
+	@media (max-width: 940px) {
+		#menu-open-btn {
+			display: inline;
+		}
+		.navigation {
+			display: flex;
+			flex-direction: column;
+			padding: 2rem;
+			box-shadow: var(--shadow);
+			background-color: var(--surface);
+			position: fixed;
+			z-index: 10;
+			top: var(--height);
+			left: 0;
+			right: 0;
+			align-items: flex-start;
+			justify-content: stretch;
+			transition: 200ms ease;
+			translate: 100% 0;
+			opacity: 0;
+		}
+		.inputs {
+			justify-content: stretch;
+			width: 100%;
+			margin-bottom: 1rem;
+		}
+		.input-field,
+		.input-field input {
+			width: 100%;
+		}
 	}
 </style>
